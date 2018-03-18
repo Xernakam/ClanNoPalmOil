13a263d20818ea04892c2b6fcadd80005217cbdd
Skoy87 <alessio.colizza@gmail.com>
2018-03-18 12:03:20 +0000

    Add files via upload
    players data frames that pull requests from API

--- ClashRoyaleClanManager.py
+++ ClashRoyaleClanManager.py
@@ -3,18 +3,42 @@ import json
 import pandas as pd
 import xlsxwriter
 import csv
-myKey= #api personal key
+myKey= 'b0cab65a56f141d28fb04a4ee29aa8f10230573afbb149f3a6bbf160a68de0a7'
 player=['8RLVV20PY']
 url='https://api.royaleapi.com/player/'+player[0]
+urlclan='https://api.royaleapi.com/clan/2GRV8JVY'
 #writer = csv.writer(open("output3.csv", "w"))
 
-
+#downloading API as .json
 headers = {
     'auth': myKey
 }
 response = requests.request('GET', url, headers=headers)
+responseclan  = requests.request('GET', urlclan, headers=headers)
+
+dataclan=responseclan.json()
 data = response.json()
 
+dataclan_pd =  pd.DataFrame(list(dataclan.items()))
+dataclan_pd.columns = ['field', 'values']
+
+listclan_fields_2_keep = ['members']
+
+clan_2_keep=dataclan_pd[dataclan_pd['field'].isin(listclan_fields_2_keep)][['values']].values.item()
+
+newsubsetclan = pd.DataFrame()
+for i in range(1,len(clan_2_keep)):
+    subsetclan = pd.DataFrame(list(clan_2_keep[i].items()))
+    subsetclan.columns = ['field', 'values']
+    newsubsetclan = newsubsetclan.append(subsetclan, ignore_index=True)
+
+clanfields_2_keep=['tag']
+clanfinal=newsubsetclan[newsubsetclan['field'].isin(clanfields_2_keep)]['values'].values
+
+print (clanfinal)
+
+
+#creating dataframe structure
 data_pd =  pd.DataFrame(list(data.items()))
 data_pd.columns = ['field', 'values']
 
@@ -32,6 +56,7 @@ for i in list_fields_2_change:
     subset['from'] = pd.Series(i,index=subset.index)
     newsubset=newsubset.append(subset,ignore_index=True)
 
-fields_2_keep = ['donations','donationsReceived']
-prova = newsubset[newsubset['field'].isin(fields_2_keep)]
-print(prova)
+fields_2_keep = ['name', 'trophies','donations','donationsReceived', 'donationsDelta','totalDonations', 'level']
+finalkeep = newsubset[newsubset['field'].isin(fields_2_keep)]
+
+print(finalkeep)
